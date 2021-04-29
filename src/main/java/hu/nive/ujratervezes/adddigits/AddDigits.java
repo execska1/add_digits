71baaee410a73b74b31c4f2b1bb03f1abc204153
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:07:57 +0200

    Implement addDigits method in AddDigits class

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,17 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        int result = 0;
+        if (input == null || input.length() == 0) {
+            result = -1;
+        } else {
+            for (char c : input.toCharArray()) {
+                if (c > '0' && c <= '9') {
+                    result += c - '0';
+                }
+            }
+        }
+        return result;
+    }
 }
